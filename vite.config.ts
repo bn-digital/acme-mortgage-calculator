e62a851405bfb1dbca7854fddc1258a005610608
Vlad Volkov <vlad@hiberbee.com>
2023-01-26 15:34:54 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({command, mode, ssrBuild}) => {
+  return {
+    base: process.env.GITHUB_REPOSITORY ?
+        `${process.env.GITHUB_REPOSITORY}/` :
+        './',
+    ...(command === 'build' ? {} : {}),
+    plugins: [react()],
+  };
+});
+
