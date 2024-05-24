5605afeec10a01416e316d7627ce4c7499bca1ad
MarynaPotiievska <marynochka93@gmail.com>
2024-05-24 16:51:33 +0300

    Fix: styles and content report page

--- src/pages/report/Report.tsx
+++ src/pages/report/Report.tsx
@@ -1,99 +1,101 @@
-import {Button, Typography} from "antd";
+import { Button, Row, Typography } from "antd";
 import './Report.scss'
 import {useGeneralContext} from "../../components/context/Context";
 import { useNavigate} from "react-router-dom";
 
 const {Title, Text, Paragraph} = Typography
 const Report = () => {
-    const navigate = useNavigate()
-    const {documentInfo} = useGeneralContext()
-    const reportInfo = [
-        {
-            title: 'Remortgage broker fee',
-            value: '£' + documentInfo.brokerFee
-        },
-        {
-            title: 'Current Lender',
-            value: documentInfo.lender
-        },
-        {
-            title: 'Mortgage Parts',
-            value: documentInfo.subAccounts
-        },
-        {
-            title: 'Repayment type',
-            value: documentInfo.repayment
-        },
-        {
-            title: 'Current balance',
-            value: documentInfo.balance
-        },
-        {
-            title: 'Remaining term in months',
-            value: documentInfo.terms
-        },
-        {
-            title: 'Index valuation',
-            value: '£' + documentInfo.valuationIndex
-        },
-        {
-            title: 'Remortgage valuation',
-            value: '£' + documentInfo.remortgageValuation
-        },
-        {
-            title: 'Product type',
-            value: documentInfo.productType
-        },
-        {
-            title: 'Initial Period',
-            value: documentInfo.initialPeriod + ' years'
-        },
-        {
-            title: 'Start date',
-            value: documentInfo.startDate
-        },
-        {
-            title: 'Exit fee',
-            value: '£' + documentInfo.exitFee
-        }
-    ]
-    const onReport = () => {
-        navigate('/pdf-report')
-        // window.print()
-    }
-    localStorage.setItem('report',JSON.stringify(reportInfo))
-    return (
-        <div className={'report-container'}>
-            <div className="report-header">
-                <Title level={3}>{'Thank you!'}</Title>
-                <Paragraph>{'Please, check out provided information:'}</Paragraph>
+  const navigate = useNavigate()
+  const {documentInfo} = useGeneralContext()
+  const reportInfo = [
+    {
+      title: 'Remortgage broker fee',
+      value: '£' + documentInfo.brokerFee
+    },
+    {
+      title: 'Current Lender',
+      value: documentInfo.lender
+    },
+    {
+      title: 'Mortgage Parts',
+      value: documentInfo.subAccounts
+    },
+    {
+      title: 'Repayment type',
+      value: documentInfo.repayment
+    },
+    {
+      title: 'Current balance',
+      value: documentInfo.balance
+    },
+    {
+      title: 'Remaining term in months',
+      value: documentInfo.terms
+    },
+    {
+      title: 'Index valuation',
+      value: '£' + documentInfo.valuationIndex
+    },
+    {
+      title: 'Remortgage valuation',
+      value: '£' + documentInfo.remortgageValuation
+    },
+    {
+      title: 'Product type',
+      value: documentInfo.productType
+    },
+    {
+      title: 'Initial Period',
+      value: documentInfo.initialPeriod + ' years'
+    },
+    {
+      title: 'Start date',
+      value: documentInfo.startDate
+    },
+    {
+      title: 'Exit fee',
+      value: '£' + documentInfo.exitFee
+    }]
+  const onReport = () => {
+    navigate('/pdf-report')
+    // window.print()
+  }
 
-            </div>
-            {reportInfo.map((it) => {
-                return (
-                    <div key={it.title} className="report-item-wrapper">
-                        <div className="title">
-                            <Text>{it.title}</Text>
-                        </div>
-                        <div className="response">
-                            <Text>{it.value}</Text>
-                        </div>
-                    </div>
-                );
-            })}
+  localStorage.setItem('report',JSON.stringify(reportInfo))
 
-            <div className="report-footer">
-                <Text>{'Your comparison report is available below'}</Text>
-                <div className="buttons-block">
-                    <Button type={'text'} size={'middle'} onClick={() => navigate(-1)}
-                            style={{marginRight: 12}}>{'Back to form'}</Button>
-
-                    <Button  type={'primary'} onClick={onReport} size={'large'}>{'View report'}</Button>
-                </div>
-            </div>
-
+  return (
+    <section>
+      <div className={'report-container'}>
+        <div className={'report-header'}>
+          <Paragraph className={'subtitle'}>{'Almost done!'}</Paragraph>
+          <Title level={2}>{'Please verify your input'}</Title>
         </div>
-    );
+        <div className={'report-body'}>
+          {reportInfo.map((it) => {
+            return (
+              <Row key={it.title} justify={'space-between'} className={'report-item-wrapper'}>
+                <Text>{it.title}</Text>
+                <Text>{it.value}</Text>
+              </Row>
+            );
+          })}
+        </div>
+        <div className={'report-footer'}>
+          <Row>
+            <Button
+              type={'text'}
+              onClick={() => navigate(-1)}
+              style={{marginRight: 12}}
+            >
+              {'Back to form'}
+            </Button>
+            <Button type={'primary'} onClick={onReport}>{'Submit'}</Button>
+          </Row>
+          <Button type={'text'}>{'Download PDF'}</Button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Report;
